d683ce186734cd8847ad23ed0fb0ca2ac9ad0ad2
Magomed <m.kostoev@tarantool.org>
2024-12-01 15:12:00 +0300

    function_alignment: add more alignments

--- function_alignment.cpp
+++ function_alignment.cpp
@@ -10,11 +10,28 @@
 	} \
 	BENCHMARK(call_##asm_fn)
 
+CREATE_BENCHMARK(int, return_0_warmup);
+
+CREATE_BENCHMARK(int, return_0_aligned_8);
+CREATE_BENCHMARK(int, return_0_aligned_16);
+CREATE_BENCHMARK(int, return_0_aligned_32);
+CREATE_BENCHMARK(int, return_0_aligned_64);
+CREATE_BENCHMARK(int, return_0_aligned_128);
+CREATE_BENCHMARK(int, return_0_aligned_256);
+CREATE_BENCHMARK(int, return_0_aligned_512);
+CREATE_BENCHMARK(int, return_0_aligned_1024);
+CREATE_BENCHMARK(int, return_0_aligned_2048);
 CREATE_BENCHMARK(int, return_0_aligned_4096);
 
+CREATE_BENCHMARK(int, return_0_cross_8);
+CREATE_BENCHMARK(int, return_0_cross_16);
 CREATE_BENCHMARK(int, return_0_cross_32);
 CREATE_BENCHMARK(int, return_0_cross_64);
 CREATE_BENCHMARK(int, return_0_cross_128);
+CREATE_BENCHMARK(int, return_0_cross_256);
+CREATE_BENCHMARK(int, return_0_cross_512);
+CREATE_BENCHMARK(int, return_0_cross_1024);
+CREATE_BENCHMARK(int, return_0_cross_2048);
 CREATE_BENCHMARK(int, return_0_cross_4096);
 
 BENCHMARK_MAIN();
--- function_alignment.s
+++ function_alignment.s
@@ -1,19 +1,121 @@
 .text
 
+.global return_0_warmup
+.global return_0_aligned_8
+.global return_0_aligned_16
+.global return_0_aligned_32
+.global return_0_aligned_64
+.global return_0_aligned_128
+.global return_0_aligned_256
+.global return_0_aligned_512
+.global return_0_aligned_1024
+.global return_0_aligned_2048
 .global return_0_aligned_4096
+.global return_0_cross_8
+.global return_0_cross_16
 .global return_0_cross_32
 .global return_0_cross_64
 .global return_0_cross_128
+.global return_0_cross_256
+.global return_0_cross_512
+.global return_0_cross_1024
+.global return_0_cross_2048
 .global return_0_cross_4096
 
+.type return_0_warmup, @function
+.type return_0_aligned_8, @function
+.type return_0_aligned_16, @function
+.type return_0_aligned_32, @function
+.type return_0_aligned_64, @function
+.type return_0_aligned_128, @function
+.type return_0_aligned_256, @function
+.type return_0_aligned_512, @function
+.type return_0_aligned_1024, @function
+.type return_0_aligned_2048, @function
 .type return_0_aligned_4096, @function
+.type return_0_cross_8, @function
+.type return_0_cross_16, @function
 .type return_0_cross_32, @function
 .type return_0_cross_64, @function
 .type return_0_cross_128, @function
+.type return_0_cross_256, @function
+.type return_0_cross_512, @function
+.type return_0_cross_1024, @function
+.type return_0_cross_2048, @function
 .type return_0_cross_4096, @function
+
+return_0_warmup:
+	xorq %rax, %rax
+	ret
+
+.align 4096, 0
+.skip 8, 0
+return_0_aligned_8:
+	xorq %rax, %rax
+	ret
+
+.align 4096, 0
+.skip 16, 0
+return_0_aligned_16:
+	xorq %rax, %rax
+	ret
+
+.align 4096, 0
+.skip 32, 0
+return_0_aligned_32:
+	xorq %rax, %rax
+	ret
+
+.align 4096, 0
+.skip 64, 0
+return_0_aligned_64:
+	xorq %rax, %rax
+	ret
+
+.align 4096, 0
+.skip 128, 0
+return_0_aligned_128:
+	xorq %rax, %rax
+	ret
+
+.align 4096, 0
+.skip 256, 0
+return_0_aligned_256:
+	xorq %rax, %rax
+	ret
+
+.align 4096, 0
+.skip 512, 0
+return_0_aligned_512:
+	xorq %rax, %rax
+	ret
+
+.align 4096, 0
+.skip 1024, 0
+return_0_aligned_1024:
+	xorq %rax, %rax
+	ret
+
+.align 4096, 0
+.skip 2048, 0
+return_0_aligned_2048:
+	xorq %rax, %rax
+	ret
 
 .align 4096, 0
 return_0_aligned_4096:
+	xorq %rax, %rax
+	ret
+
+.align 4096, 0
+.skip 7, 0
+return_0_cross_8:
+	xorq %rax, %rax
+	ret
+
+.align 4096, 0
+.skip 15, 0
+return_0_cross_16:
 	xorq %rax, %rax
 	ret
 
@@ -32,6 +134,30 @@ return_0_cross_64:
 .align 4096, 0
 .skip 127, 0
 return_0_cross_128:
+	xorq %rax, %rax
+	ret
+
+.align 4096, 0
+.skip 255, 0
+return_0_cross_256:
+	xorq %rax, %rax
+	ret
+
+.align 4096, 0
+.skip 511, 0
+return_0_cross_512:
+	xorq %rax, %rax
+	ret
+
+.align 4096, 0
+.skip 1023, 0
+return_0_cross_1024:
+	xorq %rax, %rax
+	ret
+
+.align 4096, 0
+.skip 2047, 0
+return_0_cross_2048:
 	xorq %rax, %rax
 	ret
 
